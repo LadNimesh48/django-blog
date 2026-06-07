38008834e81804077602f1ae58d56654cbb0314c
LadNimesh48 <lad.nimesh1@gmail.com>
2026-06-07 14:27:11 +0530

    post by category

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
-
+from blogs.models import Category, Blog
 
 def home(request):
-    return render(request, 'home.html')
+
+    # implemented in Context Processor
+    # categories = Category.objects.all()
+    # print(categories);
+
+    featured_posts = Blog.objects.filter(is_featured=True, status='Published').order_by('created_at')
+    # print(featured_posts);
+
+    posts = Blog.objects.filter(is_featured=False, status='Published')
+
+    context = {
+        # 'categories'        : categories,
+        'featured_posts'    : featured_posts,
+        'posts'             : posts
+    }
+    return render(request, 'home.html', context)
--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import redirect, render,get_object_or_404
+
+from blogs.models import Blog, Category
+
 
 # Create your views here.
+
+
+def post_by_category(request, category_id):
+
+    # implemented in Context Processor
+    # categories = Category.objects.all() 
+    # print(categories.query)
+
+    # one Way to handle error if not getting 
+    category_name = get_object_or_404(Category, id=category_id)
+    
+    # Second way to handle if category not found
+    # try:
+    #     # category_name = categories.filter(id=category_id).first()
+    #     category_name = Category.objects.get(id=category_id)
+    # except:
+    #     # redirect the user to homepage
+    #     return redirect('home')
+    
+    posts = Blog.objects.filter(status='Published', category =category_id)
+    # print(posts.query)
+
+    context = {
+        # 'categories'        : categories,
+        'posts'             : posts,
+        'category_name'     : category_name
+    }
+    return render(request, 'post_by_category.html', context)
+    
+    
