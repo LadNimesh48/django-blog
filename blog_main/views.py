c120ef92eb64bad83f8d835e617141d774591657
LadNimesh48 <lad.nimesh1@gmail.com>
2026-06-07 23:31:21 +0530

    login and Logout Featur

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
 from blogs.models import Category, Blog
 from assignments.models import About, SocialLink
+
+from .forms import RegistrationForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import auth
 
 def home(request):
 
@@ -19,9 +23,6 @@ def home(request):
     except:
         about = None
 
-
-
-
     context = {
         # 'categories'        : categories,
         'featured_posts'    : featured_posts,
@@ -29,3 +30,53 @@ def home(request):
         'about'             : about,
     }
     return render(request, 'home.html', context)
+
+
+def register(request):
+
+    if request.method == 'POST':
+        # print(request.method)
+        # print(request)
+        registrationForm = RegistrationForm(request.POST)
+        if registrationForm.is_valid():
+            registrationForm.save()
+            return redirect('login')
+    else:
+        registrationForm = RegistrationForm()
+
+    context = {
+        'registrationForm'  : registrationForm,
+    }
+
+    return render(request, 'register.html', context)
+
+def login(request):
+
+    if request.method == 'POST':
+
+        loginForm = AuthenticationForm(request, request.POST)
+        if loginForm.is_valid():
+            username = loginForm.cleaned_data['username']
+            password = loginForm.cleaned_data['password']
+
+            user = auth.authenticate(username=username, password=password)
+            if user is not None:
+                auth.login(request, user)
+
+            return redirect('home')
+    else:
+
+        loginForm = AuthenticationForm()
+
+    context = {
+        'loginForm'  : loginForm,
+    }
+
+    return render(request, 'login.html', context)
+
+def logout(request):
+    
+    auth.logout(request)
+
+    return redirect('login')
+
