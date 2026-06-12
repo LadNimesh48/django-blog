2ffad573d94b05e8745de7b39cacf3babc15aa31
LadNimesh48 <lad.nimesh1@gmail.com>
2026-06-13 00:40:34 +0530

    dashboard

---
+++ dashboards/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+from blogs.models import Blog, Category
+
+from django.contrib.auth.decorators import login_required
+
+
+# Create your views here.
+@login_required(login_url='login')
+def dashboard(request):
+
+    category_count = Category.objects.all().count()
+    posts_count = Blog.objects.all().count()
+
+    context = {
+        'category_count' : category_count,
+        'posts_count' : posts_count
+    }
+
+
+    return render(request, 'dashboards/dashboard.html', context)
